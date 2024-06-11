e9a8dcd15058644940cd3dcbf1538c19d6f0a023
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-06-11 15:08:55 +0100

    added basic scanner

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var path = "C:\\Users\\adunderdale\\test.txt";
+        var lines = File.ReadAllText(path);
+
+        Scanner scanner = new(source: lines);
+        var tokens = scanner.Scan();
+
+        foreach (var token in tokens) {
+            Console.WriteLine(token.ToString());
+        }
     }
 }
---
+++ Scanner.cs
@@ -0,0 +1,113 @@
+﻿namespace judas_script;
+
+public enum TokenType
+{
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH, 
+
+    BAD, 
+    WHITESPACE,
+    NEWLINE,
+    EOF
+}
+
+public class Token 
+{
+    public TokenType Type;
+    public object Value;
+    public string Text;
+
+    public Token(TokenType type, object value, string text)
+    {
+        Type = type;
+        Value = value;
+        Text = text;
+    }
+
+    public override string ToString()
+    {
+        int typeWidth = 10;
+        int valueWidth = 10;
+        int textWidth = 10;
+
+        string typeStr = Type.ToString().PadRight(typeWidth);
+        string valueStr = (Value?.ToString() ?? "null").PadRight(valueWidth);
+        string textStr = Text.PadRight(textWidth);
+
+        return $"{typeStr} | value: {valueStr} | text: {textStr}";
+    }
+}
+
+internal class Scanner
+{
+    private readonly List<Token> Tokens = new();
+    private readonly string Source;
+
+    public int Start;
+    public int End;
+
+    public int Current;
+
+    public Scanner(string source) 
+    {
+        Source = source;
+    }
+
+    public List<Token> Scan() 
+    {
+        while (!IsEndOfFile()) 
+        {
+            ScanToken();
+        }
+
+        AddToken(TokenType.EOF);
+
+        return Tokens;
+    }
+
+    public void ScanToken()
+    {
+        if (IsEndOfFile())
+            return;
+
+        switch (Source[Current])
+        {
+            case '+': AddToken(TokenType.PLUS);
+                break;
+
+            case '-': AddToken(TokenType.MINUS);
+                break;
+
+            case '*': AddToken(TokenType.STAR);
+                break;
+
+            case '/': AddToken(TokenType.SLASH);
+                break;
+
+            case ' ': AddToken(TokenType.WHITESPACE);
+                break;
+
+            case '\r':
+                if (Current + 1 < Source.Length && Source[Current + 1] == '\n')
+                    Current++;
+
+                AddToken(TokenType.NEWLINE);
+                break;
+
+            default: AddToken(TokenType.BAD); 
+                break;
+        }
+
+        Current++;
+    }
+
+    public void AddToken(TokenType type)
+    {
+        string text = (Current < Source.Length) ? Source[Current].ToString() : "";
+        Tokens.Add(new Token(type, null, text));
+    }
+
+    public bool IsEndOfFile() => Current >= Source.Length;
+}
